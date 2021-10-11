4cfedf50a2b65763e57cee7e8548983bb455dd3f
Maxim Zykov <maxim_zykov@inbox.ru>
2021-10-11 22:41:01 +0300

    Testing simple logic

--- test_sapper.py
+++ test_sapper.py
@@ -8,3 +8,20 @@ class Test(TestCase):
         assert len(cl.cells) == pow(SIZE[0] // CELL_WIDTH, 2)
         for cell in cl.cells:
             assert isinstance(cell, Cell) is True
+
+    def test_create_bomb(self):
+        cl = create_field()
+        create_bomb()
+        bomb_count = 0
+        for cell in cl.cells:
+            if cell.bomb:
+                bomb_count += 1
+        assert bomb_count == BOMBS_COUNT
+
+    def test_count_labels(self):
+        cl = create_field()
+        create_bomb()
+        for cell in sample(cl.cells, k=15):
+            cell.set_label()
+
+        assert count_labels() == 15
