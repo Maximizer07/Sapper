25baee63c265c946dbbf2d27c2c99fb11fb86939
Maxim Zykov <maxim_zykov@inbox.ru>
2021-10-11 22:49:35 +0300

    Update logic

--- sapper.py
+++ sapper.py
@@ -1,3 +1,4 @@
+import sys
 from random import sample
 import pygame
 
@@ -6,6 +7,7 @@ pygame.init()
 SIZE = 600, 650
 CELL_WIDTH = 60
 BOMBS_COUNT = 10
+CELL_FONT = pygame.font.SysFont("microsofttalie", 40)
 FONT50 = pygame.font.SysFont("microsofttalie", 50)
 FONT20 = pygame.font.SysFont("microsofttalie", 20)
 
@@ -25,8 +27,64 @@ class Cell:
 
         self.cells.append(self)
 
+    def check_cursor(self):
+        x, y = pygame.mouse.get_pos()
+        if self.x <= x < (self.x + CELL_WIDTH):
+            if self.y <= y < (self.y + CELL_WIDTH):
+                return True
+
+    def check_click(self, event):
+        if self.opened:
+            return None
+
+        x, y = self.x, self.y
+        if x <= event.pos[0] < (x + CELL_WIDTH):
+            if y <= event.pos[1] < (y + CELL_WIDTH):
+                buttons = pygame.mouse.get_pressed(num_buttons=3)
+                if buttons[0] and not self.label:
+                    if self.bomb:
+                        self.boom()
+                    else:
+                        self.open()
+                    return True
+
+                elif buttons[2]:
+                    self.set_label()
+    def open(self):
+        if self.label:
+            return None
+
+
+
+        self.color = (153, 153, 153)
+        self.color_border = (128, 128, 128)
+        self.opened = True
+
+
+        return self.opened
+
+    def set_label(self):
+        if self.opened:
+            return self.label
+        if not self.label:
+            self.label = "!"
+            self.color_label = (255, 0, 0)
+        else:
+            self.label = None
+            self.color_label = None
+        return self.label
+
+
+    def boom(self):
+        self.color = (0, 0, 0)
+        self.color_border = (0, 0, 0)
+        self.opened = True
+
     def draw(self):
-        color = self.color
+        if self.check_cursor():
+            color = list(map(lambda x: x // 1.5, self.color))
+        else:
+            color = self.color
         pygame.draw.rect(screen, color,
                          (self.x, self.y,
                           CELL_WIDTH, CELL_WIDTH),
@@ -63,6 +121,9 @@ def draw_interface():
     bomb = pygame.image.load('storage/bomb.png')
     bomp_surf = pygame.transform.scale(bomb, (40, 40))
     screen.blit(bomp_surf, (50, y + 5, 40, 40))
+    text = str(BOMBS_COUNT - count_labels())
+    follow = CELL_FONT.render(text, True, (0, 0, 0))
+    screen.blit(follow, (100, y + 12))
 
     text = "Нажмите любую кнопку "
     follow = FONT20.render(text, True, (0, 0, 0))
@@ -86,14 +147,30 @@ def count_labels():
 
 def main():
     global screen
+    global game_over
     screen = pygame.display.set_mode(SIZE)
     pygame.display.set_caption("Sapper")
     clock = pygame.time.Clock()
     create_field()
+    create_bomb()
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                for cel in Cell.cells:
+                    if cel.check_click(event) and cel.bomb:
+                        pass
+            elif event.type == pygame.KEYDOWN:
+                Cell.cells = []
+                create_field()
+                create_bomb()
+
         for cel in Cell.cells:
             cel.draw()
         draw_interface()
+
+
         pygame.display.update()
         clock.tick(30)
 
